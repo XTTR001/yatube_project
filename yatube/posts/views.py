5b64aaecc977e9702f5ceaddd1eddbc9e6026e6b
Антон <romanenkoquery@yandex.ru>
2022-12-26 19:49:09 +0300

    Добавил возможность создавать/редактировать посты, изменение пароля

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,11 @@
-from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 
-from posts.models import Group, Post
+
+from posts.forms import PostForm
+from posts.models import Group, Post, User
 
 
 def get_page_obj(request, objects, items_per_page):
@@ -36,3 +38,71 @@ def group_posts(request: HttpRequest, slug: str) -> HttpResponse:
         context={'page_obj': page_obj, 'group': group},
     )
 
+
+def profile(request, username) -> HttpResponse:
+    author = get_object_or_404(User, username=username)
+    post_list = author.posts.select_related(
+        'group'
+    )
+
+    page_obj = get_page_obj(request, post_list, 10)
+
+    return render(
+        request,
+        'posts/profile.html',
+        context={'page_obj': page_obj, 'author': author}
+    )
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post.objects.select_related(
+        'author',
+        'group',
+    ), pk=post_id)
+    author_id = post.author.id
+
+    return render(
+        request,
+        'posts/post_detail.html',
+        {
+            'post': post,
+            'author_posts_count': Post.objects.filter(
+                author=author_id,
+            ).count()
+         }
+    )
+
+@login_required
+def post_create(request):
+    form = PostForm(request.POST or None)
+
+    if not form.is_valid():
+        return render(request, 'posts/create_post.html', {'form': form})
+
+    post = form.save(commit=False)
+    post.author = request.user
+    post.save()
+    return redirect(f'/profile/{post.author.username}/')
+
+
+@login_required
+def post_edit(request, post_id):
+
+    post = get_object_or_404(Post.objects.select_related(
+        'author',
+        'group',
+    ), pk=post_id)
+
+    if request.user != post.author:
+        return redirect(f'/posts/{post_id}/')
+
+    form = PostForm(instance=post, data=request.POST or None)
+
+    if not form.is_valid():
+        return render(
+            request,
+            'posts/create_post.html',
+            {'form': form, 'is_edit': True, 'post_id': post_id})
+
+    form.save()
+    return redirect(f'/posts/{post_id}/')
--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.views import LogoutView, LoginView, PasswordResetView, PasswordResetDoneView
+from django.contrib.auth import views
 from django.urls import path
-from .views import SignUp
+from users.views import SignUp
 
 app_name = 'users'
 
@@ -8,23 +8,54 @@ urlpatterns = [
     path('signup/', SignUp.as_view(), name='signup'),
     path(
          'logout/',
-         LogoutView.as_view(template_name='users/logged_out.html'),
+         views.LogoutView.as_view(template_name='users/logged_out.html'),
          name='logout'
     ),
     path(
         'login/',
-        LoginView.as_view(template_name='users/login.html'),
+        views.LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
     path(
-        'password_reset/done',
-        PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        'password_reset/',
+        views.PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'
+        ),
+        name='password_reset'
+    ),
+    path(
+        'password_reset/done/',
+        views.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ),
         name='password_reset_done'
     ),
     path(
-        'password_reset/',
-        PasswordResetView.as_view(template_name='users/password_reset_form.html'),
-        name='password_reset_form'
+        'reset/<uidb64>/<token>/',
+        views.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
     ),
-
+    path(
+        'reset/done/',
+        views.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
+    path(
+        'password_change/',
+        views.PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'
+        ),
+        name='password_change'
+    ),
+    path(
+        'password_change/done/',
+        views.PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'
+        ),
+        name='password_change_done'
+    ),
 ]
